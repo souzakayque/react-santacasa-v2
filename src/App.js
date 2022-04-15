f741393786d01c7f9b7ba14c20111a6aaabbff23
souzakayque <52177811+souzakayque@users.noreply.github.com>
2022-04-15 14:51:11 -0300

    criando icone fixo do telefone

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Router from "./router/router";
 
 import WhatsAppIco from "./assets/images/icons/whatsapp.ico";
+import PhoneIco from "./assets/images/icons/phone.ico";
 
 import "./assets/styles/global.css";
 
@@ -9,10 +10,18 @@ function App() {
   return (
     <div>
       <div className="container-whatsapp">
-        <span>Fale com o corretor!</span>
-        <a href="https://api.whatsapp.com/send?phone=+5511982626977&text=Gostaria%20de%20cotar%20um%20plano%20da%20Santa%20Casa%20de%20Mau%C3%A1">
-          <img src={WhatsAppIco} alt="WhatsApp" title="WhatsApp" />
-        </a>
+        <div className="whatsapp-box">
+          <span className="phone-popup">Ligue para o corretor!</span>
+          <a href="tel:43214743">
+            <img src={PhoneIco} alt="WhatsApp" title="WhatsApp" />
+          </a>
+        </div>
+        <div className="whatsapp-box">
+          <span className="whatsapp-popup">Fale com o corretor!</span>
+          <a href="https://api.whatsapp.com/send?phone=+5511982626977&text=Gostaria%20de%20cotar%20um%20plano%20da%20Santa%20Casa%20de%20Mau%C3%A1">
+            <img src={WhatsAppIco} alt="WhatsApp" title="WhatsApp" />
+          </a>
+        </div>
       </div>
       <Router />
     </div>
